c520037135b0f802ac0ee199c21c769e941f441c
Elsie Oduor <eoatieno@gmail.com>
2024-10-10 17:06:32 +0300

    Made website

--- components/Menu.js
+++ components/Menu.js
@@ -7,13 +7,13 @@ const MenuSection = () => {
           <h2 className='text-3xl mb-4 font-bold'>Our Menu</h2>
             <div className="container mx-auto flex flex-col md:flex-row justify-evenly ">
                 <div className="relative w-full md:w-1/2 mb-4 cursor-pointer mr-6" onClick={() => window.location.href = '/menu'}>
-                    <Image src="/path/to/menu-image.jpg" alt="Menu" layout="responsive" width={500} height={300} className="rounded-lg"/>
+                    <Image src="/food2.jpg" alt="Menu" layout="responsive" width={500} height={300} className="rounded-lg"/>
                     <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 rounded-lg">
                         <span className="text-white text-xl font-bold">Menu</span>
                     </div>
                 </div>
                 <div className="relative w-full md:w-1/2 mb-4 cursor-pointer" onClick={() => window.location.href = '/kids-menu'}>
-                    <Image src="/path/to/kids-menu-image.jpg" alt="Kids Menu" layout="responsive" width={500} height={300} className="rounded-lg"/>
+                    <Image src="/anime.jpg" alt="Kids Menu" layout="responsive" width={500} height={300} className="rounded-lg"/>
                     <div className="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 rounded-lg">
                         <span className="text-white text-xl font-bold">Kids Menu</span>
                     </div>
